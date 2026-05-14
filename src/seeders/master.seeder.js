1132f34f7c916aca11ecd798a4365cc88463c21d
Harshad Makwana <merndev9@precisio.tech>
2026-05-14 13:59:04 +0530

    fixed

--- src/seeders/master.seeder.js
+++ src/seeders/master.seeder.js
@@ -34,6 +34,18 @@ const seedMasters = async () => {
           short_form: "25-26",
           short_form_2_digit: "26",
         },
+        {
+          id: 3,
+          name: "2026 - 27",
+          short_form: "26-27",
+          short_form_2_digit: "27",
+        },
+        {
+          id: 4,
+          name: "2027 - 28",
+          short_form: "27-28",
+          short_form_2_digit: "28",
+        },
       ],
       GradeMaster: [
         {
@@ -48,10 +60,35 @@ const seedMasters = async () => {
           short_form: "LY 2",
           display_order: 2,
         },
+        {
+          id: 3,
+          name: "Learning Year 3",
+          short_form: "LY 3",
+          display_order: 3,
+        },
+        {
+          id: 4,
+          name: "Grade 1",
+          short_form: "G1",
+          display_order: 4,
+        },
+        {
+          id: 5,
+          name: "Grade 2",
+          short_form: "G2",
+          display_order: 5,
+        },
+        {
+          id: 6,
+          name: "Grade 3",
+          short_form: "G3",
+          display_order: 6,
+        },
       ],
       GenderMaster: [
         { id: 1, name: "Female", short_form: "FE", display_order: 1 },
         { id: 2, name: "Male", short_form: "MA", display_order: 2 },
+        { id: 3, name: "Other", short_form: "OT", display_order: 3 },
       ],
       SessionMaster: [
         { id: 1, session_name: "Feb" },
@@ -59,10 +96,16 @@ const seedMasters = async () => {
         { id: 3, session_name: "May" },
       ],
       BoardMaster: [
-        { id: 2, board_code: "BRD-1", board_name: "BOB201" },
-        { id: 3, board_code: "BRD-2", board_name: "BOB203" },
-        { id: 7, board_code: "BRD-3", board_name: "BOB301" },
+        { id: 1, board_code: "BRD-001", board_name: "CBSE" },
+        { id: 2, board_code: "BRD-002", board_name: "CIE" },
+        { id: 3, board_code: "BRD-003", board_name: "CISCE" },
+        { id: 4, board_code: "BRD-004", board_name: "KSEAB" },
       ],
+      // BoardMaster: [
+      //   { id: 2, board_code: "BRD-1", board_name: "BOB201" },
+      //   { id: 3, board_code: "BRD-2", board_name: "BOB203" },
+      //   { id: 7, board_code: "BRD-3", board_name: "BOB301" },
+      // ],
       TermMaster: [
         { id: 1, term_name: "Term 1", short_form: "T1" },
         { id: 2, term_name: "Term 2", short_form: "T2" },
@@ -75,7 +118,6 @@ const seedMasters = async () => {
         { id: 1, name: "Science", short_form: "SCI" },
         { id: 2, name: "Commerce", short_form: "COM" },
       ],
-      // same as current ui verified by harshad
       BloodGroupMaster: [
         { id: 1, name: "A+", display_order: 1 },
         { id: 2, name: "A-", display_order: 2 },
@@ -86,7 +128,6 @@ const seedMasters = async () => {
         { id: 7, name: "AB+", display_order: 7 },
         { id: 8, name: "AB-", display_order: 8 },
       ],
-      // same as current ui verified by harshad
       ReligionMaster: [
         { id: 1, name: "Hindu", display_order: 1 },
         { id: 2, name: "Muslim", display_order: 2 },
@@ -97,7 +138,6 @@ const seedMasters = async () => {
         { id: 7, name: "Zorastrian", display_order: 7 },
         { id: 8, name: "Jewish", display_order: 8 },
       ],
-      // same as current ui verified by harshad
       CastMaster: [
         { id: 1, name: "General", short_form: "GR", display_order: 1 },
         { id: 2, name: "OBC", short_form: "OB", display_order: 2 },
@@ -105,7 +145,6 @@ const seedMasters = async () => {
         { id: 4, name: "ST", short_form: "ST", display_order: 4 },
         { id: 5, name: "Others", short_form: "OT", display_order: 5 },
       ],
-      // same as current ui verified by harshad
       MotherTongueMaster: [
         { id: 1, name: "Hindi", display_order: 1 },
         { id: 2, name: "English", display_order: 2 },
