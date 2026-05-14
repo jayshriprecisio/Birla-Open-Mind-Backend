18822048957c7fbd6259fcfb0d61e6173f974738
Harshad Makwana <merndev9@precisio.tech>
2026-05-14 13:05:54 +0530

    fixed

--- src/repository/student-admissions.repository.js
+++ src/repository/student-admissions.repository.js
@@ -1,6 +1,16 @@
-const { StudentAdmissions, School, GradeMaster, SchoolEnquiry, sequelize } = require('../models');
-const { Op } = require('sequelize');
-const { GenderMaster, BoardMaster, ModeOfPaymentMaster } = require('../models/masters.model');
+const {
+  StudentAdmissions,
+  School,
+  GradeMaster,
+  SchoolEnquiry,
+  sequelize,
+} = require("../models");
+const { Op } = require("sequelize");
+const {
+  GenderMaster,
+  BoardMaster,
+  ModeOfPaymentMaster,
+} = require("../models/masters.model");
 
 const createAdmissionRepo = async (data) => {
   return await StudentAdmissions.create(data);
@@ -12,25 +22,45 @@ const getAllAdmissionsRepo = async (args) => {
   const { count, rows } = await StudentAdmissions.findAndCountAll({
     where,
     include: [
-      { model: School, as: 'school', attributes: ['school_name', 'school_code'] },
-      { model: GenderMaster, as: 'gender', attributes: ['name', 'short_form'] },
-      { model: GradeMaster, as: 'grade_applying_for', attributes: ['name', 'short_form'] },
-      { model: GradeMaster, as: 'grade', attributes: ['name', 'short_form'] },
-      { model: BoardMaster, as: 'board', attributes: ['board_name', 'board_code'] },
-      { model: ModeOfPaymentMaster, as: 'payment_mode', attributes: ['mode_of_payment_name', 'name_on_receipt'] },
-      { model: SchoolEnquiry, as: 'enquiry', attributes: ['enquiry_no', 'enquiry_id'] }
+      {
+        model: School,
+        as: "school",
+        attributes: ["school_name", "school_code"],
+      },
+      { model: GenderMaster, as: "gender", attributes: ["name", "short_form"] },
+      {
+        model: GradeMaster,
+        as: "grade_applying_for",
+        attributes: ["name", "short_form"],
+      },
+      { model: GradeMaster, as: "grade", attributes: ["name", "short_form"] },
+      {
+        model: BoardMaster,
+        as: "board",
+        attributes: ["board_name", "board_code"],
+      },
+      {
+        model: ModeOfPaymentMaster,
+        as: "payment_mode",
+        attributes: ["mode_of_payment_name", "name_on_receipt"],
+      },
+      {
+        model: SchoolEnquiry,
+        as: "enquiry",
+        attributes: ["enquiry_no", "enquiry_id"],
+      },
     ],
-    order: [['created_at', 'DESC']],
+    order: [["created_at", "DESC"]],
     limit: args.limit,
     offset: (args.page - 1) * args.limit,
-    distinct: true
+    distinct: true,
   });
 
   return {
     rows,
     total: count,
     page: args.page,
-    limit: args.limit
+    limit: args.limit,
   };
 };
 
@@ -38,21 +68,41 @@ const getAdmissionByIdRepo = async (id) => {
   return await StudentAdmissions.findOne({
     where: { id: id, is_deleted: false },
     include: [
-      { model: School, as: 'school' },
-      { model: GenderMaster, as: 'gender' },
-      { model: GradeMaster, as: 'grade_applying_for' },
-      { model: GradeMaster, as: 'grade' },
-      { model: BoardMaster, as: 'board' },
-      { model: ModeOfPaymentMaster, as: 'payment_mode' },
-      { model: SchoolEnquiry, as: 'enquiry' }
-    ]
+      {
+        model: School,
+        as: "school",
+        attributes: ["school_name", "school_code"],
+      },
+      { model: GenderMaster, as: "gender", attributes: ["name", "short_form"] },
+      {
+        model: GradeMaster,
+        as: "grade_applying_for",
+        attributes: ["name", "short_form"],
+      },
+      { model: GradeMaster, as: "grade", attributes: ["name", "short_form"] },
+      {
+        model: BoardMaster,
+        as: "board",
+        attributes: ["board_name", "board_code"],
+      },
+      {
+        model: ModeOfPaymentMaster,
+        as: "payment_mode",
+        attributes: ["mode_of_payment_name", "name_on_receipt"],
+      },
+      {
+        model: SchoolEnquiry,
+        as: "enquiry",
+        attributes: ["enquiry_no", "enquiry_id"],
+      },
+    ],
   });
 };
 
 const updateAdmissionRepo = async (id, data) => {
   const admission = await StudentAdmissions.findByPk(id);
   if (!admission || admission.is_deleted) return null;
-  
+
   return await admission.update(data);
 };
 
@@ -62,7 +112,7 @@ const deleteAdmissionRepo = async (id, deletedBy) => {
 
   return await admission.update({
     is_deleted: true,
-    updated_by: deletedBy
+    updated_by: deletedBy,
   });
 };
 
@@ -71,5 +121,5 @@ module.exports = {
   getAllAdmissionsRepo,
   getAdmissionByIdRepo,
   updateAdmissionRepo,
-  deleteAdmissionRepo
+  deleteAdmissionRepo,
 };
